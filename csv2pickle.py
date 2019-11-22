23c1bcacae344db5d685f80b41dff6809bd8f65b
bhushanjain143 <48012976+bhushanjain143@users.noreply.github.com>
2019-11-22 06:51:44 +0530

    Update csv2pickle.py

--- csv2pickle.py
+++ csv2pickle.py
@@ -1,13 +1,13 @@
 import pickle
 import pandas as pd
 
-example = pd.read_csv('C:\\Users\\Rishabh\\Desktop\\New folder\\New folder\\bank-additional.csv')
-#pd.read_csv('C:\\Users\\Rishabh\\Desktop\\New folder\\New folder\\n.csv') 
-pickle_out = open("C:\\Users\\Rishabh\\Desktop\\New folder\\New folder\\dict.pickle","wb")
+example = pd.read_csv('C:\\Users\\Bhushan\\Desktop\\New folder\\New folder\\bank-additional.csv')
+#pd.read_csv('C:\\Users\\Bhushan\\Desktop\\New folder\\New folder\\n.csv') 
+pickle_out = open("C:\\Users\\Bhushan\\Desktop\\New folder\\New folder\\dict.pickle","wb")
 pickle.dump(example,pickle_out)
 pickle_out.close()
 
-pickle_in = open("C:\\Users\\Rishabh\\Desktop\\New folder\\New folder\\dict.pickle","rb")
+pickle_in = open("C:\\Users\\Bhushan\\Desktop\\New folder\\New folder\\dict.pickle","rb")
 example_dict = pickle.load(pickle_in)
 
 print(example_dict)
